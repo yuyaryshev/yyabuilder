dbd2a4368b6fd46ab08f300af6954646c20a00fd
Yuri Yaryshev <yuyaryshev@gmail.com>
2023-11-22 06:16:15 +0300

    333

--- src/projmeta.ts
+++ src/projmeta.ts
@@ -1,2 +1,2 @@
-export const version = '3.2.2';
+export const version = '4.0.0';
 export const packageName = 'yyabuilder';
--- src/ycplmon/index.ts
+++ src/ycplmon/index.ts
@@ -7,6 +7,7 @@ export * from "./readDirRecursive.js";
 export * from "./strPosToRC.js";
 export * from "./strRef.js";
 export * from "./string_funcs.js";
+export * from "./writeFileIfChanged.js";
 export * from "./yb_types.js";
 export * from "./ycplmonLib.js";
 // @INPRINT_END
